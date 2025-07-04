7ab7719f4bdaaf4f953c97b46bd4c53e7f204d07
Fernando Meira <fmeira.filho@gmail.com>
2025-07-03 22:56:32 -0600

    added dedent for generated queries

--- src/agents/utils/build_final_query.py
+++ src/agents/utils/build_final_query.py
@@ -1,13 +1,15 @@
+from textwrap import dedent
+
 from src.types import ResearchParams
 
 
 def build_final_query(params: ResearchParams) -> str:
-    final_query = f"""
+    final_query = dedent(f"""
         Find me as many leads as possible for the following query:
         
         Who: {params.who_query}
         What is the field of study: {params.what_query}
         Where are they located: {params.where_query}
         Additional context: {params.context_query}
-        """
+        """)
     return final_query
